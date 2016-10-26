ea25f860fc853abea9cfc03153ed755efa2938c4
toru inamori <toru_inamori@albert2005.co.jp>
2016-10-26 18:05:16 +0900

    図2.7(λを変えたときのヒストグラム上での当てはまりの良さと対数尤度関数の値の比較)を出してみた。

--- Statistics_Introduction_to_Modeling_for_data_analysis/chapter2/2_4_ex.R
+++ Statistics_Introduction_to_Modeling_for_data_analysis/chapter2/2_4_ex.R
@@ -1,3 +1,19 @@
 logL <- function(m) sum(dpois(data, m, log = TRUE))
 lambda <- seq(2, 5, 0.1)
 plot(lambda, sapply(lambda, logL), type = "l")
+
+plot_hist_and_prob <- function(m_list) {
+  par(mfrow=c(length(m_list)/2,2))
+  for (m in m_list){
+    print(m)
+    # 平均mのポアソン分布を作成
+    y <- 0:9
+    prob <- dpois(y, lambda = m)
+    
+    hist(data, breaks = seq(-0.5, 9.5, 1), main = paste0("lambda = ", as.character(m),
+                                                         "\nlogL = ", as.character(logL(m))))
+    lines(y, prob*length(data), type = "b", lty = 2)
+  }
+}
+
+plot_hist_and_prob(c(2,3,3.56,4))
