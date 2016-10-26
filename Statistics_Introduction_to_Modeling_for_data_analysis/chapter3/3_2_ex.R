d2dcfa5b94fca9baae6c93a4baa6f52a5fdadf30
toru inamori <toru_inamori@albert2005.co.jp>
2016-10-26 19:27:17 +0900

    データ型の確認やデータの要約の確認を追加

--- Statistics_Introduction_to_Modeling_for_data_analysis/chapter3/3_2_ex.R
+++ Statistics_Introduction_to_Modeling_for_data_analysis/chapter3/3_2_ex.R
@@ -2,6 +2,12 @@ setwd("./Statistics_Introduction_to_Modeling_for_data_analysis/chapter3/")
 d <- read.csv("data3a.csv")
 
 head(d, 10)
-print(d$x)
 print(d$y)
+print(d$x)
 print(d$f)
+
+print(class(d$y))
+print(class(d$x))
+print(class(d$f))
+
+summary(d)
