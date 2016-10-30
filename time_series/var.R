3b5591148a394f2dc2a2041bdf26d3af3cc5c4e6
inaty <inaty0610@gmail.com>
2016-10-30 22:21:31 +0900

    adf検定で単位根か確認

--- time_series/var.R
+++ time_series/var.R
@@ -1,5 +1,7 @@
 library("vars", lib.loc="~/R/win-library/3.3")
 library("tsDyn", lib.loc="~/R/win-library/3.3")
+library("tseries", lib.loc="~/R/win-library/3.3")
+
 data(Canada)
 VARselect(Canada,lag.max=4)
 
@@ -9,3 +11,9 @@ plot(Canada.var)
 
 Canada.predict <- predict(Canada.var,n.ahead = 20,ci = 0.95)
 plot(Canada.predict)
+
+adf.test(Canada[,1])
+adf.test(Canada[,2])
+adf.test(Canada[,3])
+adf.test(Canada[,4])
+
