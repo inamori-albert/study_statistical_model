380d81b46e2d2583f19702d4759ecb99151a62e4
toru inamori <toru_inamori@albert2005.co.jp>
2016-10-31 12:49:00 +0900

    コメントを付けた

--- Statistics_Introduction_to_Modeling_for_data_analysis/chapter6/6_1_ex.R
+++ Statistics_Introduction_to_Modeling_for_data_analysis/chapter6/6_1_ex.R
@@ -1,5 +1,7 @@
+# データの読み込み
 d <- read.csv("data4a.csv")
 
+# データの確認
 summary(d)
 plot(d$x, d$y, xlab = "xi", ylab = "yi", pch = c(21,19)[d$f])
 legend("topleft", legend = c("C", "T"), pch = c(21,19))
