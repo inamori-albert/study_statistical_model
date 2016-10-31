fb0e3b977547c570cf588a0c7ec5c2bf2ccd7984
toru inamori <toru_inamori@albert2005.co.jp>
2016-10-31 15:47:57 +0900

    2項分布でGLM

--- Statistics_Introduction_to_Modeling_for_data_analysis/chapter7/7_1_ex.R
+++ Statistics_Introduction_to_Modeling_for_data_analysis/chapter7/7_1_ex.R
@@ -2,3 +2,12 @@
 d <- read.csv("data.csv")
 
 plot(d$x, d$y)
+
+# 2項分布でGLM
+fit <- glm(cbind(y, N-y) ~ x, data = d, family = binomial(link = "logit"))
+summary(fit)
+
+xx <- seq(min(d$x), max(d$x), length = 100)
+nn <- 8
+yy <- predict(fit, newdata = data.frame(x=xx), type = "response") * nn
+lines(xx, yy, type = "b", lwd = 2)
