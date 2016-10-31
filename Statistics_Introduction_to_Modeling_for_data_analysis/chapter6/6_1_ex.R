cd99f2085f8636a111bbec3263457f86f61acd23
toru inamori <toru_inamori@albert2005.co.jp>
2016-10-31 14:39:00 +0900

    2項分布を使ったGLM

--- Statistics_Introduction_to_Modeling_for_data_analysis/chapter6/6_1_ex.R
+++ Statistics_Introduction_to_Modeling_for_data_analysis/chapter6/6_1_ex.R
@@ -5,3 +5,17 @@ d <- read.csv("data4a.csv")
 summary(d)
 plot(d$x, d$y, xlab = "xi", ylab = "yi", pch = c(21,19)[d$f])
 legend("topleft", legend = c("C", "T"), pch = c(21,19))
+
+# パラメータ推定
+fit.bino <- glm(cbind(y, N-y) ~ x + f, data = d, family = binomial(link = "logit"))
+summary(fit.bino)
+
+# 推定結果の図示
+nn <- 8
+xx <- seq(min(d$x), max(d$x), length = 100)
+ff <- c("C")
+yy <- predict(fit.bino, newdata = data.frame(x = xx, f = ff), type = "response") * nn
+lines(xx, yy, type = "b", lwd = 2, pch = c(21))
+ff <- c("T")
+yy <- predict(fit.bino, newdata = data.frame(x = xx, f = ff), type = "response") * nn
+lines(xx, yy, type = "b", lwd = 2, pch = c(19))
