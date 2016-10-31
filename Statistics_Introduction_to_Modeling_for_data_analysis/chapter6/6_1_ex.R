0b80abe24dcd890bdf60f65c9c7166a63884d91f
toru inamori <toru_inamori@albert2005.co.jp>
2016-10-31 14:43:16 +0900

    AICの値でモデルを検証

--- Statistics_Introduction_to_Modeling_for_data_analysis/chapter6/6_1_ex.R
+++ Statistics_Introduction_to_Modeling_for_data_analysis/chapter6/6_1_ex.R
@@ -1,3 +1,5 @@
+library(MASS)
+
 # データの読み込み
 d <- read.csv("data4a.csv")
 
@@ -19,3 +21,15 @@ lines(xx, yy, type = "b", lwd = 2, pch = c(21))
 ff <- c("T")
 yy <- predict(fit.bino, newdata = data.frame(x = xx, f = ff), type = "response") * nn
 lines(xx, yy, type = "b", lwd = 2, pch = c(19))
+
+# AICによるモデル選択
+fit.1 <- glm(cbind(y, N-y) ~ 1, data = d, family = binomial(link = "logit"))
+fit.f <- glm(cbind(y, N-y) ~ f, data = d, family = binomial(link = "logit"))
+fit.x <- glm(cbind(y, N-y) ~ x, data = d, family = binomial(link = "logit"))
+fit.xf <- glm(cbind(y, N-y) ~ x + f, data = d, family = binomial(link = "logit"))
+stepAIC(fit.1)
+stepAIC(fit.f)
+stepAIC(fit.x)
+stepAIC(fit.xf)
+# fit.xfが一番AICが低くなるのでよい
+
