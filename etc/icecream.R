d07fd2c8be7c84de735e410307637945a37ab37b
inaty <inaty0610@gmail.com>
2016-11-04 23:14:07 +0900

    描画部分を追加

--- etc/icecream.R
+++ etc/icecream.R
@@ -1,4 +1,5 @@
 library(rstan)
+require("coda")
 
 icecream <- read.csv("icecream.txt")
 
@@ -10,3 +11,15 @@ war<-5000
 ite<-100000
 cha<-1
 fit<-stan(file = scr, model_name = scr, data = dat, pars = par, chains = cha, warmup = war, iter=ite)
+
+s <- mcmc.list(lapply(1:ncol(fit), function(x) mcmc(as.array(fit)[,x,])))
+S <- ggs(s)
+
+ggs_histogram(S)
+ggs_traceplot(S)
+ggs_density(S)
+ggs_compare_partial(S)
+ggs_running(S)
+ggs_autocorrelation(S)
+ggs_crosscorrelation(S)
+ggs_caterpillar(S)
