afb44b6fcbfcffa451159bb80b0b54c1dd2c36bb
toru inamori <toru_inamori@albert2005.co.jp>
2016-12-07 17:04:11 +0900

    時系列データの扱いに関する部分を追加

--- etc/analysis_tmp.R
+++ etc/analysis_tmp.R
@@ -62,3 +62,30 @@ pareto.chart(z)
 
 # 種別で色分け
 with(iris, plot(Petal.Length, Species, col=rainbow(7)[Species]))
+
+with(iris, plot(Petal.Length, Sepal.Length, col=rainbow(7)[Species]))
+
+# 時系列データ
+ts.plot(ldeaths,mdeaths,fdeaths,col=c(1,2,3))
+# 単位根検定→両方とも単位根なので、一回微分しないと見せかけの回帰が生じる
+adf.test(ldeaths)
+adf.test(mdeaths)
+
+library(forecast)
+tsdisplay(ldeaths)
+tsdisplay(mdeaths)
+
+my_arima <- auto.arima(ldeaths, max.p = 10, max.q = 10, max.order = 30, stepwise=F, trace=T, seasonal = F)
+plot(forecast(my_arima, h = 30))
+
+data("Canada")
+# 単位根検定→単位根でないので、そのままVAR使ってOK
+adf.test(Canada[,1])
+adf.test(Canada[,2])
+adf.test(Canada[,3])
+adf.test(Canada[,4])
+
+library(vars)
+my_var = VAR(Canada,p=VARselect(Canada)$selection[1])
+summary(my_var)
+plot(forecast(my_var, h = 10))
